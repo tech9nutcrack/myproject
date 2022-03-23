36bcedc4bdbadac4eef652f7d034e40cfa7c774f
tech9nutcrack <sauravapr942001@gmail.com>
2022-03-23 10:29:42 +0530

    versionun

--- myapp/models.py
+++ myapp/models.py
@@ -1,3 +1,10 @@
+from operator import mod
 from django.db import models
 
 # Create your models here.
+class user(models.Model):
+    username = models.CharField(max_length=255)
+    password = models.CharField(max_length=255)
+    fullname = models.CharField(max_length=255)
+    age = models.CharField(max_length=255)
+    address = models.TextField()
